32bfbe8f51674b1c9e9be24d343ca1c515b4c1d9
djakonystar <djakonystar911@gmail.com>
2023-01-31 16:27:28 +0500

    Fixed problems with backstack

--- app/src/main/java/uz/texnopos/contactsandroid4/ui/MainActivity.kt
+++ app/src/main/java/uz/texnopos/contactsandroid4/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package uz.texnopos.contactsandroid4.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.FragmentManager
 import uz.texnopos.contactsandroid4.R
 import uz.texnopos.contactsandroid4.databinding.ActivityMainBinding
 import uz.texnopos.contactsandroid4.ui.all.AllContactsFragment
@@ -17,23 +19,57 @@ class MainActivity : AppCompatActivity() {
 
         supportFragmentManager.beginTransaction()
             .add(R.id.fragment_container, AllContactsFragment())
+            .addToBackStack(null)
             .commit()
 
         binding.apply {
             bnvMain.setOnItemSelectedListener { item ->
                 when (item.itemId) {
                     R.id.item_all -> {
+                        supportFragmentManager.popBackStack(
+                            null,
+                            FragmentManager.POP_BACK_STACK_INCLUSIVE
+                        )
+
                         supportFragmentManager.beginTransaction()
                             .replace(R.id.fragment_container, AllContactsFragment())
+                            .addToBackStack(null)
                             .commit()
                     }
                     R.id.item_favorites -> {
+                        supportFragmentManager.popBackStack(
+                            null,
+                            FragmentManager.POP_BACK_STACK_INCLUSIVE
+                        )
+
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.fragment_container, AllContactsFragment())
+                            .addToBackStack(null)
+                            .commit()
+
                         supportFragmentManager.beginTransaction()
                             .replace(R.id.fragment_container, FavoriteContactsFragment())
+                            .addToBackStack("favorites")
                             .commit()
                     }
                 }
                 true
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+
+        val backStack = supportFragmentManager
+            .getBackStackEntryAt(supportFragmentManager.backStackEntryCount - 1)
+
+        when (backStack.name) {
+            null -> {
+                binding.bnvMain.selectedItemId = R.id.item_all
+            }
+            "favorites" -> {
+                binding.bnvMain.selectedItemId = R.id.item_favorites
             }
         }
     }
